046b78f8ce34bdfae602666699ca4b5863576787
Marcelo Diament <marcelodiament@gmail.com>
2020-11-17 21:51:31 -0300

    agora passando HTML ao invés de plain text

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require('http')
 
 http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain'
+    'Content-Type': 'HTML'
   })
   console.log('Servidor rodando corretamente!')
-  res.end('Servidor funcionando novamente! Agora de um jeitinho mais correto!')
+  res.end('<h1>Servidor rodando!</h1><h2>Uhuuuu!</h2>')
 }).listen(3030, 'localhost')
