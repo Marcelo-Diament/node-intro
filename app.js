24513ef694fbc9563910212487551bc35a848537
Marcelo Diament <marcelodiament@gmail.com>
2020-11-17 21:49:20 -0300

    escrevendo head para passas um status code e tipo do content

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-  res.end('Servidor funcionando!')
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  })
+  console.log('Servidor rodando corretamente!')
+  res.end('Servidor funcionando novamente! Agora de um jeitinho mais correto!')
 }).listen(3030, 'localhost')
